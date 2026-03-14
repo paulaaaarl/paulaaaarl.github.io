fdc0c25f7202a508d5151bf84bb36610306cdeda
paulaaaarl <paula.rodriguezlagos@gmail.com>
2026-03-14 13:52:49 -0300

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
- site: 'paulaaaarl.github.io',
+ site: 'https://paulaaaarl.github.io',
  vite: {
    plugins: [tailwindcss()],
  }
