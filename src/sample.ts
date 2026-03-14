ecb0282881cbdb258a7dafb8a48993235fa6bf7b
paulaaaarl <paula.rodriguezlagos@gmail.com>
2026-03-14 13:24:05 -0300

    first commit

---
+++ src/sample.ts
@@ -0,0 +1,15 @@
+const author = {
+  name: `Paula`,
+  nickname: `Paula`,
+  tiktok: `https://www.tiktok.com/@indieteamofone`,
+  ig: `https://www.instagram.com/indieteamofone/`,
+  x: `https://x.com/indieteamofone`,
+  email: "mailto:anthonylannn@gmail.com",
+};
+
+const buy = {
+  title: `See on Github`,
+  link: `https://github.com/anthonylan/angie`,
+};
+
+export { author, buy };
